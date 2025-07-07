36d126eda68128ce2419944f4af3532cb5e42745
Burmistrov Kirill <burmistrovkirill.03.12.1993@gmail.com>
2025-07-07 21:30:05 +0700

    'sidebarAnimationBugfix'

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -35,10 +35,12 @@ export function Sidebar({
     <div
       className={`bg-bg fixed top-0 left-0 z-60 flex h-screen flex-col md:relative ${
         sidebarIsOpen ? "w-50 translate-x-0" : "w-0 -translate-x-50"
-      } border-border border-r-2 transition-all duration-200`}
+      } border-border border-r-2 transition-all duration-300`}
     >
-      <div className="border-border bg-bg-secondary flex h-15 w-full items-center justify-between border-b-2">
-        <h1 className="text-button ml-2 text-2xl">Gym Dairy</h1>
+      <div className="border-border bg-bg-secondary flex h-15 w-full items-center justify-between overflow-hidden border-b-2">
+        <h1 className="text-button nowrap ml-2 flex w-30 text-2xl whitespace-nowrap">
+          Gym Dairy
+        </h1>
         <PiTextAlignRight
           onClick={() => {
             setModalIsOpen(false);
@@ -47,12 +49,12 @@ export function Sidebar({
           className="text-text-main active:text-button-hover hover:text-button-hover mr-5 size-10 cursor-pointer transition-colors duration-300"
         />
       </div>
-      <ul className="text-text-main flex w-50 flex-col text-2xl">
+      <ul className="text-text-main flex flex-col text-2xl">
         <li
           onClick={(e) => {
             openTapHandler(e);
           }}
-          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Home" ? "bg-bg-secondary border-border border-r-2" : "border-0"} hover:border-t-bg-secondary flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-r-2 hover:border-b-2`}
+          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Home" ? "bg-bg-secondary border-border" : "border-0"} hover:border-t-bg-secondary flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-b-2`}
         >
           <FaHome className="text-button" />
           Home
@@ -61,7 +63,7 @@ export function Sidebar({
           onClick={(e) => {
             openTapHandler(e);
           }}
-          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Workout" ? "bg-bg-secondary border-border border-r-2" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-r-2`}
+          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Workout" ? "bg-bg-secondary border-border" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2`}
         >
           <GiGymBag className="text-button" />
           Workout
@@ -70,7 +72,7 @@ export function Sidebar({
           onClick={(e) => {
             openTapHandler(e);
           }}
-          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Diet" ? "bg-bg-secondary border-border border-r-2" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-r-2`}
+          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Diet" ? "bg-bg-secondary border-border" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2`}
         >
           <IoFastFoodOutline className="text-button" />
           Diet
@@ -79,7 +81,7 @@ export function Sidebar({
           onClick={(e) => {
             openTapHandler(e);
           }}
-          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Plan" ? "bg-bg-secondary border-border border-r-2" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-r-2`}
+          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Plan" ? "bg-bg-secondary border-border" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2`}
         >
           <PiBookFill className="text-button" />
           Plan
@@ -88,7 +90,7 @@ export function Sidebar({
           onClick={(e) => {
             openTapHandler(e);
           }}
-          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Statistics" ? "bg-bg-secondary border-border border-r-2" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2 hover:border-r-2`}
+          className={`hover:bg-bg-secondary active:bg-bg-secondary hover:border-border ${openTab === "Statistics" ? "bg-bg-secondary border-border" : "border-0"} flex h-13 w-full cursor-pointer items-center gap-1 pl-2 select-none hover:border-y-2`}
         >
           <IoIosStats className="text-button" />
           Statistics
@@ -97,7 +99,7 @@ export function Sidebar({
       <div className="flex grow flex-col items-center justify-end pb-10">
         <button
           onClick={switchThemeHandler}
-          className="bg-bg-secondary active:bg-button-hover border-border text-text-main hover:bg-button-hover transition-color w-5/10 cursor-pointer rounded-xl border-2 py-2 text-2xl duration-300 select-none active:scale-96"
+          className="bg-bg-secondary active:bg-button-hover border-border text-text-main hover:bg-button-hover transition-color w-5/10 cursor-pointer overflow-hidden rounded-xl border-2 py-2 text-2xl duration-300 select-none active:scale-96"
         >
           Theme
         </button>
